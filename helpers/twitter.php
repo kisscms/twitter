a3aeed82e63f2cd6f86ca3b13baad72a95f3ccb9
Makis Tracend <makis.tracend@gmail.com>
2012-10-14 04:25:12 -0700

    #1 adding basic GET method

--- helpers/twitter.php
+++ helpers/twitter.php
@@ -2,27 +2,47 @@
 /* Twitter for KISSCMS */
 class Twitter {
 	
-	private $key;
-	private $secret;
-	private $token;
 	private $api;
-	private $me;
 	private $oauth;
+	private $config;
+	private $creds;
 	private $cache;
 	
 	function  __construct() {
+		// main URL
+		$this->api = "https://api.twitter.com/1.1/";
 		
-		$this->key = $GLOBALS['config']['twitter']['key'];
-	 	$this->secret = $GLOBALS['config']['twitter']['secret'];
-		$this->me = ( empty($_SESSION['access']['twitter']['user_id']) ) ? false : $_SESSION['access']['twitter']['user_id'];
-	 	$this->token = ( empty($_SESSION['access']['twitter']['access_token']) ) ? false : $_SESSION['access']['twitter']['access_token'];
-	 	$this->api = "https://api.twitter.com/1.1/";
-		$this->oauth = "https://api.twitter.com/oauth/";
-		$this->cache = $this->getCache();
+		// load all the necessery subclasses
+		$this->oauth = new Twitter_OAuth();
+		
+		$this->config = $GLOBALS['config']['twitter'];
+		// get/update the creds
+		$this->creds = $this->oauth->creds();
+		
 	}
 	
 	// REST methods
-	function  get() {
+	function  get( $service="", $params=array() ){
+		
+		// check cache before....
+		//...
+		
+		if( $this->creds['access_token'] ) {
+			// add access_token
+			$params['access_token'] = $this->creds['access_token'];
+		} else {
+			// add secret
+			//$params['access_token'] = $this->creds['access_token'];
+		}
+		
+		$url = $this->api . $service;
+		
+		$http = new Http();
+		$http->setMethod('GET');
+		$http->setParams( $params );
+		$http->execute( $url );
+		
+		return ($http->error) ? die($http->error) : json_decode( $http->result);
 		
 	}
 	

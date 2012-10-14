b161319e756cc31fae9ed3b93eb597d1f0b3eadd
Makis Tracend <makis.tracend@gmail.com>
2012-10-14 02:08:17 -0700

    Updating the path of the dependency class

--- helpers/twitter_oauth.php
+++ helpers/twitter_oauth.php
@@ -1,6 +1,6 @@
 <?php
 // FIX - to include the base OAuth lib not in alphabetical order
-require_once( realpath("../") . "/app/plugins/oauth/helpers/kiss_oauth.php" );
+require_once( APP . "plugins/oauth/helpers/kiss_oauth.php" );
 
 /* Discus for KISSCMS */
 class Twitter_OAuth extends KISS_OAuth_v1 {
